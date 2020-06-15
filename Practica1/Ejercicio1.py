54912d56d2bd3768fa4977118270860044d980de
paulopez1330 <49596062+paulopez1330@users.noreply.github.com>
2020-06-15 18:53:05 -0300

    Create Ejercicio1.py

---
+++ Practica1/Ejercicio1.py
@@ -0,0 +1,4 @@
+base = float(input('Ingrese base del trinangulo: '))
+altura = float(input('Ingrese altura del trinangulo: '))
+superficie = base * altura
+print( 'Superficie del triangulo: ' + str(superficie))
